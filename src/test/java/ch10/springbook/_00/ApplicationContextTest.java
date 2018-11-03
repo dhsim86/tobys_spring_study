263a2b7557223713d244f25ae5079092b190f77b
Dongho Sim <dhsim86@gmail.com>
2018-11-03 14:29:47 +0900

    [ch10] BeanDefinition Register Test

--- src/test/java/ch10/springbook/_00/ApplicationContextTest.java
+++ src/test/java/ch10/springbook/_00/ApplicationContextTest.java
@@ -1,9 +1,12 @@
 package ch10.springbook._00;
 
 import org.junit.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.support.StaticApplicationContext;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
@@ -16,7 +19,16 @@ public class ApplicationContextTest {
 
         Hello hello1 = sc.getBean("hello1", Hello.class);
 
+        BeanDefinition helloDef = new RootBeanDefinition(Hello.class);
+        helloDef.getPropertyValues().addPropertyValue("name", "Spring");
+        sc.registerBeanDefinition("hello2", helloDef);
+
         assertThat(hello1, is(notNullValue()));
+
+        Hello hello2 = sc.getBean("hello2", Hello.class);
+        assertThat(hello2.sayHello(), is("Hello Spring"));
+        assertThat(hello1, is(not(hello2)));
+        assertThat(sc.getBeanFactory().getBeanDefinitionCount(), is(2));
     }
 
 }
